b60a5222dfb8f6e29f8ee5f6eb0a29b15513e2fc
vonpupp <vonpupp@gmail.com>
2015-06-29 14:50:37 -0300

    Review of dotfiles

--- 10-install.sh
+++ 10-install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # PIP (USER)
-pip install --user git+git://github.com/Lokaltog/powerline
+#pip install --user git+git://github.com/Lokaltog/powerline
--- 30-config-ycm.sh
+++ 30-config-ycm.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Compile YCM
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
+#cd ~/.vim/bundle/YouCompleteMe
+#cd ~/.vim/bundle/YouCompleteMe
+git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe --depth 1 --recursive
+#git submodule update --init --recursive
 ##./install.sh --clang-completer
 ./install.sh
