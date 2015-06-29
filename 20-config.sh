88b7d071bae2d909573dd1205f839c2460e87f4e
vonpupp <vonpupp@gmail.com>
2015-06-29 15:19:05 -0300

    wget molokai.vim manually for bootstrap to work automatically

--- 20-config.sh
+++ 20-config.sh
@@ -6,6 +6,9 @@ mkdir -p ~/.vim/config
 mkdir -p ~/.vim/spell
 mkdir -p ~/.vim/syntax
 
+# Add molokai
+wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O ~/.vim/colors/molokai.vim
+
 # Clone vundle
 git clone https://github.com/gmariK/vundle.git ~/.vim/bundle/vundle
 
