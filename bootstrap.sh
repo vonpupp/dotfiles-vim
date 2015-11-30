1a0b7f482aa16f71d2c8523c0504c2b2afefaa36
vonpupp <vonpupp@gmail.com>
2015-11-30 00:33:49 -0200

    Fix autowrap with molokai and ycm

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,7 @@ source ~/.homesick/repos/homeshick/homeshick.sh
 homeshick link $REPO_NAME
 
 # YOUCOMPLETEME
-git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe --depth 1 --recursive
-~/.vim/bundle/YouCompleteMe/install.sh
+git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe --depth 1 --recursive ~/.vim/bundle/YouCompleteMe/install.sh
 
 # VIM
 mkdir -p ~/.vim/bundle
@@ -17,8 +16,7 @@ mkdir -p ~/.vim/spell
 mkdir -p ~/.vim/syntax
 
 # COLORS
-wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
--O ~/.vim/colors/molokai.vim
+wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O ~/.vim/colors/molokai.vim
 
 # VUNDLE
 git clone https://github.com/gmariK/vundle.git ~/.vim/bundle/vundle
